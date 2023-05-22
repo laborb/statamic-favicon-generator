9fdb09527d4050a5e4666b3dbe8527168d0b7167
jf <jf@laborb.de>
2023-05-22 14:09:31 +0200

    Adjust docs callout

--- resources/views/cp/settings/index.blade.php
+++ resources/views/cp/settings/index.blade.php
@@ -1,9 +1,9 @@
 @extends('statamic::layout')
-@section('title', __('favicon-generator::cp.general.headline'))
+@section('title', __('statamic-favicon-generator::cp.general.headline'))
 
 @section('content')
     <favicon-generator
-        title="@lang('favicon-generator::cp.general.headline')"
+        title="@lang('statamic-favicon-generator::cp.general.headline')"
         :blueprint='@json($blueprint)'
         :meta='@json($meta)'
         :initial-values='@json($values)'
@@ -11,6 +11,6 @@
 
     @include('statamic::partials.docs-callout', [
 		'topic' => 'Favicon Generator',
-		'url' => 'https://laborb.de'
+		'url' => 'https://statamic.com/addons/laborb/favicon-generator'
 	])
 @endsection
